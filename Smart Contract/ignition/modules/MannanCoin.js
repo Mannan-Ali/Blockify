59d20c39be28e53979b341fb699f08aa2534b754
mannan <alimannan.am244@gmail.com>
2025-01-07 18:22:06 +0530

    Project Complete

--- Smart Contract/ignition/modules/MannanCoin.js
+++ Smart Contract/ignition/modules/MannanCoin.js
@@ -1,10 +1,6 @@
 // This setup uses Hardhat Ignition to manage smart contract deployments.
 // Learn more about it at https://hardhat.org/ignition
 
-
-
-// MannanCoinApp#MannanCoin_ICO - 0x5FbDB2315678afecb367f032d93F642f64180aa3
-
 // $ npx hardhat ignition deploy ignition/modules/MannanCoin.js --network localhost
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
